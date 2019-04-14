318b9cd35ccbdff1b8b4f8d16d03d695f43da85f
John Bussey <jbussey365@yahoo.com>
2019-04-14 10:49:45 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <Box v-for="(color, key) in boxColors" :key="key" :color="color" @click="clickBox(key)"></Box>
+
+    <p>Red Count: {{ redCount }}</p>
+    <p>Blue Count: {{ blueCount }}</p>
+  </div>
+</template>
+
+<script>
+import Box from './components/Box.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Box
+  },
+  methods: {
+    clickBox: function(i){
+      var newColor = this.boxColors[i] == "red" ? "blue" : "red";
+      this.$set(this.boxColors, i, newColor);
+    }
+  },
+  computed: {
+    redCount: function(){
+      var count = 0;
+      this.boxColors.forEach(function(color){
+        if(color == "red"){
+          count ++;
+        }
+      });
+      return count;
+    },
+    blueCount: function(){
+      var count = 0;
+      this.boxColors.forEach(function(color){
+        if(color == "blue"){
+          count ++;
+        }
+      });
+      return count;
+    }
+  },
+  data: function(){
+    return {
+      boxColors: [
+        "blue",
+        "blue",
+        "blue",
+        "blue",
+        "blue",
+        "blue"
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Box.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :style="{ backgroundColor: this.color }" @click="$emit('click')">
+    <h3>click me</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['color']
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  div {
+    width: 100px;
+    height: 200px;
+    border: 2px solid grey;
+    margin: 15px;
+    padding: 10px;
+    display: inline-block;
+    box-sizing: border-box;
+  }
+</style>
